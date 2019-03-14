d63cf391922cfd272eea4d2b4c9e6311552202bf
Stanko Culaja <stanko.culaja@kolotree.com>
2019-03-14 18:22:13 +0100

    Simple refactoring.

--- CommandSide/DomainServices/Queueing/CommandHandlers/RecallCustomerHandler.cs
+++ CommandSide/DomainServices/Queueing/CommandHandlers/RecallCustomerHandler.cs
@@ -5,16 +5,16 @@ using Common.Messaging;
 
 namespace CommandSide.DomainServices.Queueing.CommandHandlers
 {
-    public sealed class OpenCounterHandler : CommandHandler<OpenCounter>
+    public sealed class RecallCustomerHandler : CommandHandler<ReCallCustomer>
     {
         private readonly ICustomerQueueRepository _repository;
 
-        public OpenCounterHandler(ICustomerQueueRepository repository)
+        public RecallCustomerHandler(ICustomerQueueRepository repository)
         {
             _repository = repository;
         }
 
-        public override Result Handle(OpenCounter c) => _repository
-            .BorrowSingle(cq => cq.OpenCounter(c.CounterId));
+        public override Result Handle(ReCallCustomer c) => _repository
+            .BorrowSingle(cq => cq.ReCallCustomerFor(c.CounterId));
     }
 }
