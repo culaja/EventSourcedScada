444d494fef6767b7cd39d590e401c5ddd8b150a7
Stanko Culaja <stanko.culaja@kolotree.com>
2019-03-17 15:54:01 +0100

    Added LargeEventStoreCreator application which populate event store with 1 000 000 customers so we can test load of application startup.

---
+++ TestApplications/LargeEventStoreCreator/CommandExecutionException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LargeEventStoreCreator
+{
+    public sealed class CommandExecutionException : Exception
+    {
+        public CommandExecutionException(string error) : base(error)
+        {
+            
+        }
+    }
+}
